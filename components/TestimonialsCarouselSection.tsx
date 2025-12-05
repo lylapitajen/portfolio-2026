192bce5e3ccb4db250d11141624c5b5f08f96120
lylapitajen <lylapitajen00@gmail.com>
2025-12-05 14:38:40 +0000

    metadata changes and added new Embla testimonial carousel

---
+++ components/TestimonialsCarouselSection.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { testimonials } from "@/app/data";
+import { Button } from "./shadcn/button";
+import { ChevronRight, ChevronLeft } from "lucide-react";
+
+export default function TestimonialsCarouselSection() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    align: "start",
+    loop: true,
+  });
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+  return (
+    <>
+      <section className="flex flex-col gap-8 py-10">
+        <div className="flex justify-between items-center pb-2 border-b">
+          <h2 className="text-sm uppercase tracking-wide text-neutral-500">
+            Testimonials
+          </h2>
+          <div className="flex gap-2 items-center">
+            <Button variant="outline" size="icon" onClick={scrollPrev}>
+              <ChevronLeft />
+            </Button>
+            <Button variant="outline" size="icon" onClick={scrollNext}>
+              <ChevronRight />
+            </Button>
+          </div>
+        </div>
+        <div className="embla overflow-hidden" ref={emblaRef}>
+          <div className="embla__container flex mr-4">
+            {testimonials.map((testimonial, i) => (
+              <div
+                className="embla__slide flex-none basis-full md:basis-1/2 lg:basis-1/3 mr-4"
+                key={i}
+              >
+                <div className="flex flex-col justify-between gap-10 bg-neutral-100 p-6 rounded-md h-full">
+                  <p className="text-lg font-medium">{testimonial.text}</p>
+                  <div className="flex gap-2 items-center">
+                    <div className="h-10 w-10 rounded-full bg-neutral-200"></div>
+                    <div>
+                      <p className="font-medium">{testimonial.author}</p>
+                      <p className="text-neutral-600">
+                        {testimonial.authorJobTitle},{" "}
+                        {testimonial.clientCompany}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
